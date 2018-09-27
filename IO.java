189080f0225f790c4233737999971cc15cce7869
Fernando0107 <35601479+Fernando0107@users.noreply.github.com>
2018-09-26 22:47:40 -0600

    Add files via upload

---
+++ IO.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+
+
+	Scanner input=new Scanner(System.in);
+
+	String User_input;
+
+	System.out.print("ingrese la palabra");
+
+	User_input=input.next();
+
+	String CAPS;
+
+	CAPS=User_input.toUpperCase();
+
+	System.out.print(CAPS);
+	}
+
+}
